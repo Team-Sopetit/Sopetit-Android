8e6f66635f714ea5eec18e90d42bfcbf19e1237d
river99 <qkrrkd42@naver.com>
2024-01-04 04:15:29 +0900

    #1 [setting] Timber Log를 Debug 상태에서만 출력하기

--- app/src/main/java/com/sopetit/softie/SoftieApplication.kt
+++ app/src/main/java/com/sopetit/softie/SoftieApplication.kt
@@ -2,9 +2,13 @@ package com.sopetit.softie
 
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
+import timber.log.Timber
+
 class SoftieApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 }
