45d995c9a761b2dc856289184f34336f6311ab9a
pump9918 <hoyeon9918@gmail.com>
2024-01-09 17:23:07 +0900

    #6 [ui] CI 오류 수정

--- app/src/main/java/com/sopetit/softie/ui/main/MainActivity.kt
+++ app/src/main/java/com/sopetit/softie/ui/main/MainActivity.kt
@@ -11,6 +11,4 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
-
-    
 }
