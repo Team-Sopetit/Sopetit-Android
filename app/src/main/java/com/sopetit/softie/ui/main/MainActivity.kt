a9946720bbf8aa665d85e398e95057e05e781992
pump9918 <hoyeon9918@gmail.com>
2024-01-09 17:16:21 +0900

    #6 [ui] Empty View Fragment 세팅

--- app/src/main/java/com/sopetit/softie/ui/main/MainActivity.kt
+++ app/src/main/java/com/sopetit/softie/ui/main/MainActivity.kt
@@ -11,4 +11,6 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
+
+    
 }
