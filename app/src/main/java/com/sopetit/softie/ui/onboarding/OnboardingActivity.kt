dfdd7ed6cd697efdbcfb848dc761fd7ed5bdd7a8
Hailey <91793891+stellar-halo@users.noreply.github.com>
2024-01-10 01:25:52 +0900

    #15 [chore] naming 변경

--- app/src/main/java/com/sopetit/softie/ui/onboarding/OnboardingActivity.kt
+++ app/src/main/java/com/sopetit/softie/ui/onboarding/OnboardingActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
 import com.sopetit.softie.R
 import com.sopetit.softie.databinding.ActivityOnboardingBinding
+import com.sopetit.softie.ui.onboarding.dollselection.DollSelectionFragment
 import com.sopetit.softie.ui.onboarding.routinechoice.RoutineChoiceFragment
 import com.sopetit.softie.ui.onboarding.themechoice.ChoiceThemeFragment
 import com.sopetit.softie.util.binding.BindingActivity
@@ -27,7 +28,7 @@ class OnboardingActivity :
         if (currentFragment == null) {
             viewModel.changeBearChoiceView()
             supportFragmentManager.beginTransaction()
-                .add(R.id.fcv_onboarding_fragment, BearChoiceFragment())
+                .add(R.id.fcv_onboarding_fragment, DollSelectionFragment())
                 .commit()
         }
     }
--- app/src/main/java/com/sopetit/softie/ui/onboarding/dollselection/DollSelectionFragment.kt
+++ app/src/main/java/com/sopetit/softie/ui/onboarding/dollselection/DollSelectionFragment.kt
@@ -1,14 +1,15 @@
-package com.sopetit.softie.ui.onboarding
+package com.sopetit.softie.ui.onboarding.dollselection
 
 import android.os.Bundle
 import android.view.View
 import androidx.lifecycle.ViewModelProvider
 import com.sopetit.softie.R
-import com.sopetit.softie.databinding.FragmentOnboardingChoiceBearBinding
+import com.sopetit.softie.databinding.FragmentOnboardingDollSelectionBinding
+import com.sopetit.softie.ui.onboarding.OnboardingViewModel
 import com.sopetit.softie.util.binding.BindingFragment
 
-class BearChoiceFragment :
-    BindingFragment<FragmentOnboardingChoiceBearBinding>(R.layout.fragment_onboarding_choice_bear) {
+class DollSelectionFragment :
+    BindingFragment<FragmentOnboardingDollSelectionBinding>(R.layout.fragment_onboarding_doll_selection) {
 
     private lateinit var viewModel: OnboardingViewModel
 
