ad30c07b25d3cab745cb23f11daedcdbb04e3bd4
KIM MINJEONG <minjeong030420@gmail.com>
2024-01-10 21:44:48 +0900

    #27 [add] splash activity 생성

---
+++ app/src/main/java/com/sopetit/softie/ui/onboarding/splash/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.sopetit.softie.ui.onboarding.splash
+
+import android.os.Bundle
+import com.sopetit.softie.R
+import com.sopetit.softie.databinding.ActivitySplashBinding
+import com.sopetit.softie.util.binding.BindingActivity
+
+class SplashActivity : BindingActivity<ActivitySplashBinding>(R.layout.activity_splash) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
