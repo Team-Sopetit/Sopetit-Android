c3d902c35509758958be502e9376696c5d13e82b
KIM MINJEONG <minjeong030420@gmail.com>
2024-01-11 04:11:21 +0900

    #30 [ui] 설정 화면

---
+++ app/src/main/java/com/sopetit/softie/ui/setting/SettingActivity.kt
@@ -0,0 +1,13 @@
+package com.sopetit.softie.ui.setting
+
+import android.os.Bundle
+import com.sopetit.softie.R
+import com.sopetit.softie.databinding.ActivitySettingBinding
+import com.sopetit.softie.util.binding.BindingActivity
+
+class SettingActivity : BindingActivity<ActivitySettingBinding>(R.layout.activity_setting) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
