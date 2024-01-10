b8cb6fa8894b4d2d3e303c1368f9202a45fa9708
Hailey <91793891+stellar-halo@users.noreply.github.com>
2024-01-11 01:43:47 +0900

    #23 [ui] editText에 filter 적용 -> 특수문자 입력 시, 경고 문구 뜨게 / 버튼 비활성화를 위한 검사 로직

---
+++ app/src/main/java/com/sopetit/softie/ui/onboarding/bearnaming/BearNamingViewModel.kt
@@ -0,0 +1,38 @@
+package com.sopetit.softie.ui.onboarding.bearnaming
+
+import android.text.InputFilter
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import java.util.regex.Pattern
+
+class BearNamingViewModel : ViewModel() {
+    private val _isNickNameValid = MutableLiveData(false)
+    val isNickNameValid: LiveData<Boolean> get() = _isNickNameValid
+
+    private val _isSpecialCharacterEntered = MutableLiveData<Boolean>()
+    val isSpecialCharacterEntered: LiveData<Boolean> = _isSpecialCharacterEntered
+
+    val nickname: MutableLiveData<String> = MutableLiveData("")
+
+    var filterSpecialCharacter = InputFilter { source, _, _, _, _, _ ->
+        if (source.isNullOrBlank() || NICKNAME_REGEX.matcher(source).matches()) {
+            _isSpecialCharacterEntered.value = false
+            source
+        } else {
+            _isSpecialCharacterEntered.value = true
+            source.filter { it.isLetterOrDigit() }
+        }
+    }
+
+    fun checkIsNicknameValid() {
+        val nicknameLength = nickname.value?.length ?: 0
+        _isNickNameValid.value = nicknameLength in 1..10
+    }
+
+    companion object {
+        private const val NICKNAME_PATTERN =
+            "^[ㄱ-ㅣ가-힣a-zA-Z0-9\\u318D\\u119E\\u11A2\\u2022\\u2025a\\u00B7\\uFE55]+$"
+        val NICKNAME_REGEX: Pattern = Pattern.compile(NICKNAME_PATTERN)
+    }
+}
