15ff7176b3ddf0fdfed13c5e7395ced9da57210d
heominhoe <hhmmm1005@naver.com>
2024-01-11 01:08:50 +0900

    [ui] 리스트 개수별 상자 띄우기

---
+++ app/src/main/java/com/sopetit/softie/ui/userdaily/DailyEmptyFragment.kt
@@ -0,0 +1,21 @@
+package com.sopetit.softie.ui.userdaily
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import com.sopetit.softie.R
+import com.sopetit.softie.databinding.FragmentDailyEmptyBinding
+import com.sopetit.softie.util.binding.BindingFragment
+
+class DailyEmptyFragment :
+    BindingFragment<FragmentDailyEmptyBinding>(R.layout.fragment_daily_empty) {
+
+    private val dailyEmptyViewModel by viewModels<DailyEmptyViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.lifecycleOwner = this
+        binding.dailyEmptyViewModel = dailyEmptyViewModel
+    }
+}
---
+++ app/src/main/java/com/sopetit/softie/ui/userdaily/DailyEmptyViewModel.kt
@@ -0,0 +1,16 @@
+package com.sopetit.softie.ui.userdaily
+
+import androidx.lifecycle.ViewModel
+
+class DailyEmptyViewModel : ViewModel() {
+    val routineAddList = listOf<Int>(0, 0, 0)
+
+    companion object {
+        const val NONE = 0
+        const val FIRST_ROUTINE = 1
+        const val SECOND_ROUTINE = 2
+        const val THIRD_ROUTINE = 3
+        const val FOURTH_ROUTINE = 4
+        const val MAX_ROUTINE = 3
+    }
+}
