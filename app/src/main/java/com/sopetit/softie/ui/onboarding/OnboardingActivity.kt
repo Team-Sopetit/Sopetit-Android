6ce2c953aee1feb5bb5d0d52088204a5de8fb71e
Hailey <91793891+stellar-halo@users.noreply.github.com>
2024-01-10 15:28:43 +0900

    #15 [chore] status bar 색상 변경

--- app/src/main/java/com/sopetit/softie/ui/onboarding/OnboardingActivity.kt
+++ app/src/main/java/com/sopetit/softie/ui/onboarding/OnboardingActivity.kt
@@ -2,6 +2,7 @@ package com.sopetit.softie.ui.onboarding
 
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.sopetit.softie.R
 import com.sopetit.softie.databinding.ActivityOnboardingBinding
@@ -19,8 +20,13 @@ class OnboardingActivity :
         super.onCreate(savedInstanceState)
         binding.viewModel = viewModel
 
+        initStatusBarColor()
         initMakeFragment()
         initChangeFragment()
+    }
+
+    private fun initStatusBarColor() {
+        window.statusBarColor = ContextCompat.getColor(this, R.color.background)
     }
 
     private fun initMakeFragment() {
