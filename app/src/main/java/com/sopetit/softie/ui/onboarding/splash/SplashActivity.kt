f3809f3f53f77e685e84f68a83746abdecb6bd4b
KIM MINJEONG <minjeong030420@gmail.com>
2024-01-11 01:09:00 +0900

    #27 [ui] splash 생성

--- app/src/main/java/com/sopetit/softie/ui/onboarding/splash/SplashActivity.kt
+++ app/src/main/java/com/sopetit/softie/ui/onboarding/splash/SplashActivity.kt
@@ -1,13 +1,37 @@
 package com.sopetit.softie.ui.onboarding.splash
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.sopetit.softie.R
 import com.sopetit.softie.databinding.ActivitySplashBinding
+import com.sopetit.softie.ui.storytelling.StoryTellingActivity
 import com.sopetit.softie.util.binding.BindingActivity
 
 class SplashActivity : BindingActivity<ActivitySplashBinding>(R.layout.activity_splash) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        initMakeFragment()
+    }
+
+    private fun initMakeFragment() {
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fcv_splash)
+        if (currentFragment == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fcv_splash, SplashVersionFirstFragment())
+                .commit()
+        }
+        initMakeSplash()
+    }
+
+    private fun initMakeSplash() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, StoryTellingActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1000)
     }
 }
