ceea026d35f2c61ba08cfaa69c4debdcbace963a
KIM MINJEONG <minjeong030420@gmail.com>
2024-01-10 17:38:24 +0900

    #17 [feat] 온보딩 루틴 선택 recyclerview 구현

--- app/src/main/java/com/sopetit/softie/ui/onboarding/routinechoice/RoutineChoiceFragment.kt
+++ app/src/main/java/com/sopetit/softie/ui/onboarding/routinechoice/RoutineChoiceFragment.kt
@@ -2,7 +2,9 @@ package com.sopetit.softie.ui.onboarding.routinechoice
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.sopetit.softie.R
 import com.sopetit.softie.databinding.FragmentOnboardingChoiceRoutineBinding
 import com.sopetit.softie.ui.onboarding.OnboardingViewModel
@@ -13,12 +15,48 @@ class RoutineChoiceFragment :
     BindingFragment<FragmentOnboardingChoiceRoutineBinding>(R.layout.fragment_onboarding_choice_routine) {
 
     private lateinit var viewModel: OnboardingViewModel
+    private val routineViewModel by viewModels<RoutineChoiceViewModel>()
+
+    private var _choiceRoutineAdapter: RoutineChoiceAdapter? = null
+    private val choiceRoutineAdapter
+        get() = requireNotNull(_choiceRoutineAdapter)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(requireActivity()).get(OnboardingViewModel::class.java)
         binding.viewModel = viewModel
+        binding.routineViewModel = routineViewModel
 
-        Timber.d("routineChoice: ${viewModel.selectedThemeArray.value}")
+        Timber.d("routineChoice: theme -> ${viewModel.selectedThemeArray.value}")
+
+        initMakeRoutineAdapter()
+    }
+
+    private fun initMakeRoutineAdapter() {
+        _choiceRoutineAdapter = RoutineChoiceAdapter()
+        binding.rvOnboardingChoiceRoutine.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = choiceRoutineAdapter
+        }
+        routineViewModel.mockRoutineList.observe(viewLifecycleOwner) {
+            choiceRoutineAdapter.submitList(it)
+        }
+
+        selectRoutines()
+    }
+
+    private fun selectRoutines() {
+        choiceRoutineAdapter.setOnRoutineClickListener {
+            Timber.d("routineChoice: routine -> ${choiceRoutineAdapter.selectedRoutineArray}")
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _choiceRoutineAdapter = null
+    }
+
+    companion object {
+        const val MAXIMUM_ROUTINE_SELECTION = 3
     }
 }
