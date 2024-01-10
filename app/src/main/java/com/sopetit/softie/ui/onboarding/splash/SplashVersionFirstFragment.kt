559e92d817ad65b50486a60b5a2f037458112ec1
KIM MINJEONG <minjeong030420@gmail.com>
2024-01-11 00:54:50 +0900

    #27 [add] 각 버전별 fragment 생성

---
+++ app/src/main/java/com/sopetit/softie/ui/onboarding/splash/SplashVersionFirstFragment.kt
@@ -0,0 +1,15 @@
+package com.sopetit.softie.ui.onboarding.splash
+
+import android.os.Bundle
+import android.view.View
+import com.sopetit.softie.R
+import com.sopetit.softie.databinding.FragmentSplashVersionFirstBinding
+import com.sopetit.softie.util.binding.BindingFragment
+
+class SplashVersionFirstFragment :
+    BindingFragment<FragmentSplashVersionFirstBinding>(R.layout.fragment_splash_version_first) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/sopetit/softie/ui/onboarding/splash/SplashVersionSecondFragment.kt
@@ -0,0 +1,15 @@
+package com.sopetit.softie.ui.onboarding.splash
+
+import android.os.Bundle
+import android.view.View
+import com.sopetit.softie.R
+import com.sopetit.softie.databinding.FragmentSplashVersionSecondBinding
+import com.sopetit.softie.util.binding.BindingFragment
+
+class SplashVersionSecondFragment :
+    BindingFragment<FragmentSplashVersionSecondBinding>(R.layout.fragment_splash_version_second) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
