f139f92612db5821d7d1869cd8da28a1919d339c
pump9918 <hoyeon9918@gmail.com>
2024-01-11 13:18:52 +0900

    #10 [ui] Content 리스트 어댑터

--- app/src/main/java/com/sopetit/softie/ui/main/happy/addlist/HappyAddListChipContentAdapter.kt
+++ app/src/main/java/com/sopetit/softie/ui/main/happy/addlist/HappyAddListChipContentAdapter.kt
@@ -1,33 +1,49 @@
 package com.sopetit.softie.ui.main.happy.addlist
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.sopetit.softie.databinding.ItemHappyAddListChipBinding
+import com.sopetit.softie.domain.entity.HappyChip
+import com.sopetit.softie.util.ItemDiffCallback
 
-class HappyAddListChipContentAdapter(context: Context) :
-    RecyclerView.Adapter<HappyAddListChipContentViewHolder>() {
-    private val inflater by lazy { LayoutInflater.from(context) }
-    private var happyAddListChipContentList: List<HappyAddListChipContent> =
-        emptyList()
+
+class HappyAddListChipContentAdapter :
+    ListAdapter<HappyChip, HappyAddListChipContentAdapter.HappyAddListChipContentViewHolder>(
+        ItemDiffCallback<HappyChip>(
+            onItemsTheSame = { oldItem, newItem -> oldItem == newItem },
+            onContentsTheSame = { oldItem, newItem -> oldItem == newItem }
+        )
+    ) {
+
+    inner class HappyAddListChipContentViewHolder(
+        private val binding: ItemHappyAddListChipBinding,
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun onBind(data: HappyChip) {
+            binding.itemHappyAddChipComponent.text = data.name
+            binding.root.setOnClickListener {
+                onItemClickListener?.let { it(data) }
+            }
+        }
+    }
+
+    private var onItemClickListener: ((HappyChip) -> Unit)? = null
+    fun setOnChipClickListener(listener: (HappyChip) -> Unit) {
+        onItemClickListener = listener
+    }
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): HappyAddListChipContentViewHolder {
-        val binding = ItemHappyAddListChipBinding.inflate(inflater, parent, false)
+        val binding = ItemHappyAddListChipBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
         return HappyAddListChipContentViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: HappyAddListChipContentViewHolder, position: Int) {
-        holder.onBind(happyAddListChipContentList[position])
-    }
-
-    override fun getItemCount() = happyAddListChipContentList.size
-
-    fun setHomeContentList(homeContentChipList: List<HappyAddListChipContent>) {
-        this.happyAddListChipContentList = homeContentChipList.toList()
-        notifyDataSetChanged()
+        holder.onBind(getItem(position))
     }
 }
