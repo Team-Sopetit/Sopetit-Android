81eda1abd586a73a95f12fc4ea4015160af6a399
Hailey <91793891+stellar-halo@users.noreply.github.com>
2024-01-12 01:36:59 +0900

    #36 [fix] status 확장 함수 추가

--- app/src/main/java/com/sopetit/softie/util/Context.kt
+++ app/src/main/java/com/sopetit/softie/util/Context.kt
@@ -5,6 +5,8 @@ import android.content.Context
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import com.google.android.material.snackbar.Snackbar
 
 fun Context.hideKeyboard(view: View) {
@@ -14,11 +16,23 @@ fun Context.hideKeyboard(view: View) {
 
 fun snackBar(
     anchorView: View,
-    message: String,
+    message: String
 ) {
     Snackbar.make(anchorView, message, Snackbar.LENGTH_SHORT).show()
 }
 
 fun Context.toast(message: String) {
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+fun Fragment.setStatusBarColor(colorResId: Int) {
+    activity?.let {
+        val statusBarColor = ContextCompat.getColor(it, colorResId)
+        it.window.statusBarColor = statusBarColor
+    }
+}
+
+fun Activity.setStatusBarColorFromResource(colorResId: Int) {
+    val statusBarColor = ContextCompat.getColor(this, colorResId)
+    window.statusBarColor = statusBarColor
 }
