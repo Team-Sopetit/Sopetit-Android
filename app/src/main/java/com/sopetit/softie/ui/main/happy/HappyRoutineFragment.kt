d075b2f89b9b78efbbe1099e23c1333bf38d0a72
Hailey <91793891+stellar-halo@users.noreply.github.com>
2024-01-11 14:45:37 +0900

    #33 [chore] binding fragment로 변경

---
+++ app/src/main/java/com/sopetit/softie/ui/main/happy/HappyRoutineFragment.kt
@@ -0,0 +1,14 @@
+package com.sopetit.softie.ui.main.happy
+
+import android.os.Bundle
+import android.view.View
+import com.sopetit.softie.R
+import com.sopetit.softie.databinding.FragmentHappyRoutineBinding
+import com.sopetit.softie.util.binding.BindingFragment
+
+class HappyRoutineFragment :
+    BindingFragment<FragmentHappyRoutineBinding>(R.layout.fragment_happy_routine) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
