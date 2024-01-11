e785c5b2b6d5e34affac84d0cbf9331b2a99c1c0
Hailey <91793891+stellar-halo@users.noreply.github.com>
2024-01-11 14:18:40 +0900

    #33 [ui] main에서 바텀 네비 클릭 시, fragment 변경 틀 추가 및 bottom navigation 글자 및

--- app/src/main/java/com/sopetit/softie/ui/main/MainActivity.kt
+++ app/src/main/java/com/sopetit/softie/ui/main/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.sopetit.softie.ui.main
 
+import android.content.res.ColorStateList
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import com.sopetit.softie.R
 import com.sopetit.softie.databinding.ActivityMainBinding
+import com.sopetit.softie.ui.main.home.HomeFragment
 import com.sopetit.softie.util.binding.BindingActivity
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -10,5 +14,61 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        initFragment()
+        setBottomNavigationTint()
+
+        binding.bnvMain.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.bottom_navigation_progress -> {
+                    // changeFragment() 진행중 화면 띄우기
+                    true
+                }
+
+                R.id.bottom_navigation_home -> {
+                    // changeFragment() 홈 화면 띄우기
+                    true
+                }
+
+                R.id.bottom_navigation_happiness_routine -> {
+                    // changeFragment() 행복 루틴 화면 띄우기
+                    true
+                }
+
+                else -> {
+                    true
+                }
+            }
+        }
+    }
+
+    private fun initFragment() {
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fcv_main)
+        if (currentFragment == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fcv_main, HomeFragment())
+                .commit()
+        }
+    }
+
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fcv_onboarding_fragment, fragment)
+            .commit()
+    }
+
+    private fun setBottomNavigationTint() {
+        binding.bnvMain.itemIconTintList = null
+
+        val selectedTextColor = ContextCompat.getColor(this, R.color.main1)
+        val unselectedTextColor = ContextCompat.getColor(this, R.color.gray200)
+
+        binding.bnvMain.itemTextColor = ColorStateList(
+            arrayOf(
+                intArrayOf(android.R.attr.state_checked),
+                intArrayOf(-android.R.attr.state_checked)
+            ),
+            intArrayOf(selectedTextColor, unselectedTextColor)
+        )
     }
 }
