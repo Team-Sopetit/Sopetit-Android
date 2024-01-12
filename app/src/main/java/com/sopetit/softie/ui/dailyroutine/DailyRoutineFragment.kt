f7af8f52445221ec5dbc6da6b4237b1625986cf5
KIM MINJEONG <minjeong030420@gmail.com>
2024-01-13 00:19:32 +0900

    #42 [ui] 데일리 루틴 리스트 ui 생성

--- app/src/main/java/com/sopetit/softie/ui/dailyroutine/DailyRoutineFragment.kt
+++ app/src/main/java/com/sopetit/softie/ui/dailyroutine/DailyRoutineFragment.kt
@@ -17,7 +17,28 @@ class DailyRoutineFragment :
 
         binding.viewModel = viewModel
 
+        initSetDailyRoutineContent()
         initSetDeleteView()
+    }
+
+    private fun initSetDailyRoutineContent() {
+        viewModel.mockDailyRoutineList.observe(viewLifecycleOwner) { dailyRoutineList ->
+            with(binding) {
+                tvDailyRoutineAddNameFirst.text = dailyRoutineList[0].content
+                tvDailyRoutineIngFirst.text = "${dailyRoutineList[0].achieveCount}번째 달성 중"
+                btnDailyRoutineYetFinFirst.setBackgroundResource(if (dailyRoutineList[0].isAchieve) R.drawable.shape_main1_fill_10_rect else R.drawable.shape_gray100_fill_10_rect)
+            }
+            with(binding) {
+                tvDailyRoutineIngSecond.text = dailyRoutineList[1].content
+                tvDailyRoutineIngSecond.text = "${dailyRoutineList[1].achieveCount}번째 달성 중"
+                btnDailyRoutineYetFinSecond.setBackgroundResource(if (dailyRoutineList[1].isAchieve) R.drawable.shape_main1_fill_10_rect else R.drawable.shape_gray100_fill_10_rect)
+            }
+            with(binding) {
+                tvDailyRoutineIngThird.text = dailyRoutineList[2].content
+                tvDailyRoutineIngThird.text = "${dailyRoutineList[2].achieveCount}번째 달성 중"
+                btnDailyRoutineYetFinThird.setBackgroundResource(if (dailyRoutineList[2].isAchieve) R.drawable.shape_main1_fill_10_rect else R.drawable.shape_gray100_fill_10_rect)
+            }
+        }
     }
 
     private fun initSetDeleteView() {
