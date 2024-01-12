93da8529010cf1b8661cbb9da72b060c9b6393f4
pump9918 <hoyeon9918@gmail.com>
2024-01-12 23:05:31 +0900

    #39 [chore] 행복 루틴 리스트 뷰와 연결

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -117,6 +117,9 @@ dependencies {
     // Splash Screen
     implementation("androidx.core:core-splashscreen:1.0.1")
 
+    // indicator
+    implementation("com.tbuonomo:dotsindicator:5.0")
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
