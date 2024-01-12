14c3e99ab09a3f2520b06af3b8a80b8d66b49418
heominhoe <hhmmm1005@naver.com>
2024-01-12 19:00:00 +0900

    #8 [feat] : 리사이클러뷰 클릭이벤트 색깔 바뀌게

--- app/src/main/java/com/sopetit/softie/ui/dailyroutine/dailyroutineadd/DailyRoutineAddActivity.kt
+++ app/src/main/java/com/sopetit/softie/ui/dailyroutine/dailyroutineadd/DailyRoutineAddActivity.kt
@@ -10,6 +10,7 @@ import androidx.viewpager2.widget.MarginPageTransformer
 import androidx.viewpager2.widget.ViewPager2
 import com.sopetit.softie.R
 import com.sopetit.softie.databinding.ActivityDailyRoutineAddBinding
+import com.sopetit.softie.domain.entity.Theme
 import com.sopetit.softie.util.binding.BindingActivity
 import timber.log.Timber
 
@@ -31,7 +32,6 @@ class DailyRoutineAddActivity :
 
     private val dailyRoutineAddViewModel by viewModels<DailyRoutineAddViewModel>()
 
-    val themeId = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,7 +41,6 @@ class DailyRoutineAddActivity :
         setupAdapter()
         setViewPager()
         setIndicator()
-//        clickTheme()
         setItemDiv()
     }
 
@@ -49,23 +48,41 @@ class DailyRoutineAddActivity :
         with(binding) {
             dailyRoutineAddCardPagerAdapter = DailyRoutineAddCardPagerAdapter()
             vpDailyRoutineAddCard.adapter = dailyRoutineAddCardPagerAdapter
-            dailyRoutineAddThemeAdapter = DailyRoutineAddThemeAdapter()
+            dailyRoutineAddThemeAdapter = DailyRoutineAddThemeAdapter().apply {
+                setOnItemClickListener(object : DailyRoutineAddThemeAdapter.OnItemClickListener {
+                    override fun onItemClick(item: Theme, position: Int) {
+                        setRoutineList(item)
+                    }
+                })
+            }
             rvDailyRoutineAddTheme.adapter = dailyRoutineAddThemeAdapter
         }
-//        with(dailyRoutineAddCardPagerAdapter) {
-//            submitList(dailyRoutineAddViewModel.mockDailyFirstCardList.value)
-////            submitList(dailyRoutineAddViewModel.mockDailySecondCardList.value)
-////            submitList(dailyRoutineAddViewModel.mockDailyThirdCardList.value)
-////            submitList(dailyRoutineAddViewModel.mockDailyFourthCardList.value)
-////            submitList(dailyRoutineAddViewModel.mockDailyFifthCardList.value)
-//        }
-//        dailyRoutineAddThemeAdapter.submitList(dailyRoutineAddViewModel.mockThemeList.value)
-
         dailyRoutineAddViewModel.mockThemeList.observe(this) {
             dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailyFirstCardList.value)
             dailyRoutineAddThemeAdapter.submitList(dailyRoutineAddViewModel.mockThemeList.value)
         }
-        clickTheme()
+    }
+
+    private fun setRoutineList(item: Theme) {
+        Timber.d("daily routine act -> ${item.themeId}")
+        when (item.themeId) {
+            1 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailyFirstCardList.value)
+            2 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailySecondCardList.value)
+            3 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailyThirdCardList.value)
+            4 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailyFourthCardList.value)
+            5 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailyFifthCardList.value)
+            6 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailySixthCardList.value)
+            7 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailySeventhCardList.value)
+            8 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailyEighthCardList.value)
+            9 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailyNinthCardList.value)
+            10 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailyTenthCardList.value)
+            11 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailyEleventhCardList.value)
+            12 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailyTwelfthCardList.value)
+
+            else -> dailyRoutineAddCardPagerAdapter.submitList(
+                dailyRoutineAddViewModel.mockDailyFirstCardList.value
+            )
+        }
     }
 
     private fun setViewPager() {
@@ -87,27 +104,27 @@ class DailyRoutineAddActivity :
         binding.diDailyRoutineAddIndicator.attachTo(binding.vpDailyRoutineAddCard)
     }
 
-    private fun clickTheme() {
-        dailyRoutineAddThemeAdapter.setOnThemeClickListener {
-            Timber.d("daily routine act -> ${it.themeId}")
-            // Timber.d("daily routine act -> ${dailyRoutineAddThemeAdapter.selectedThemeArray}")
-            when (it.themeId) {
-                1 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailyFirstCardList.value)
-                2 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailySecondCardList.value)
-                3 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailyThirdCardList.value)
-                4 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailyFourthCardList.value)
-                5 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailyFifthCardList.value)
-                6 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailySixthCardList.value)
-                7 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailySeventhCardList.value)
-                8 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailyEighthCardList.value)
-                9 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailyNinthCardList.value)
-                10 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailyTenthCardList.value)
-                11 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailyEleventhCardList.value)
-                12 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailyTwelfthCardList.value)
-                else -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailyFirstCardList.value)
-            }
-        }
-    }
+//    private fun clickTheme() {
+//        dailyRoutineAddThemeAdapter.set {
+//            Timber.d("daily routine act -> ${it.themeId}")
+//            // Timber.d("daily routine act -> ${dailyRoutineAddThemeAdapter.selectedThemeArray}")
+//            when (it.themeId) {
+//                1 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailyFirstCardList.value)
+//                2 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailySecondCardList.value)
+//                3 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailyThirdCardList.value)
+//                4 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailyFourthCardList.value)
+//                5 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailyFifthCardList.value)
+//                6 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailySixthCardList.value)
+//                7 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailySeventhCardList.value)
+//                8 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailyEighthCardList.value)
+//                9 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailyNinthCardList.value)
+//                10 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailyTenthCardList.value)
+//                11 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailyEleventhCardList.value)
+//                12 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailyTwelfthCardList.value)
+//                else -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailyFirstCardList.value)
+//            }
+//        }
+//    }
 
     class HorizontalItemDecorator(private val divHeight: Int) : RecyclerView.ItemDecoration() {
         @Override
--- app/src/main/java/com/sopetit/softie/ui/dailyroutine/dailyroutineadd/DailyRoutineAddThemeAdapter.kt
+++ app/src/main/java/com/sopetit/softie/ui/dailyroutine/dailyroutineadd/DailyRoutineAddThemeAdapter.kt
@@ -2,6 +2,7 @@ package com.sopetit.softie.ui.dailyroutine.dailyroutineadd
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.AdapterView.OnItemClickListener
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -10,7 +11,6 @@ import com.sopetit.softie.R
 import com.sopetit.softie.databinding.ItemDailyRoutineAddThemeBinding
 import com.sopetit.softie.domain.entity.Theme
 import com.sopetit.softie.util.ItemDiffCallback
-import timber.log.Timber
 
 class DailyRoutineAddThemeAdapter :
     ListAdapter<Theme, DailyRoutineAddThemeAdapter.DailyThemeViewHolder>(
@@ -19,46 +19,85 @@ class DailyRoutineAddThemeAdapter :
             onContentsTheSame = { old, new -> old == new }
         )
     ) {
-    private val selectedPositions = HashSet<Int>()
+    private var onItemClickListener: OnItemClickListener? = null
+    private var selectedPosition = 0
 
-    var selectedThemeArray = arrayListOf<Int>()
-
-    private var onItemClickListener: ((Theme) -> Unit)? = null
-    fun setOnThemeClickListener(listener: (Theme) -> Unit) {
-        onItemClickListener = listener
+    interface OnItemClickListener {
+        fun onItemClick(item: Theme, position: Int)
     }
 
-    private fun isSelected(theme: Theme) = selectedPositions.contains(theme.themeId)
+    fun setOnItemClickListener(listener: OnItemClickListener) {
+        this.onItemClickListener = listener
+    }
 
+    //    private val selectedPositions = HashSet<Int>()
+//    private var onItemClickListener: ((Theme) -> Unit)? = null
+//    fun setOnThemeClickListener(listener: (Theme) -> Unit) {
+//        onItemClickListener = listener
+//    }
+//
+//    private var selectedPosition = RecyclerView.NO_POSITION
+//    private var prePosition = RecyclerView.NO_POSITION
+//
+//    private fun isSelected(theme: Theme) = selectedPositions.contains(theme.themeId)
+//
     inner class DailyThemeViewHolder(private val binding: ItemDailyRoutineAddThemeBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun onBind(data: Theme) {
             binding.tvDailyRoutineAddThemeName.text = data.name
             binding.ivDailyRoutineAddThemeIcon.load(data.iconImageUrl)
-            binding.flDailyRoutineAddTheme.isSelected = isSelected(data)
-            binding.root.setOnClickListener {
-                selectTheme(data)
-//                notifyDataSetChanged()
-                onItemClickListener?.let { it(data) }
-                Timber.d("daily routine -> ${selectedPositions}")
+            if (selectedPosition == absoluteAdapterPosition) {
+                changeThemeBackground(binding, true)
+//            binding.flDailyRoutineAddTheme.isSelected = isSelected(data)
+//            binding.root.setOnClickListener {
+//                selectTheme(binding, data)
+//                //notifyDataSetChanged()
+//                onItemClickListener?.let { it(data) }
+//                Timber.d("daily routine -> ${selectedPositions}")
+            } else {
+                changeThemeBackground(binding, false)
+            }
+
+            if (onItemClickListener != null) {
+                binding.root.setOnClickListener {
+                    onItemClickListener?.onItemClick(data, absoluteAdapterPosition)
+                    if (selectedPosition != absoluteAdapterPosition) {
+                        changeThemeBackground(binding, true)
+                        notifyItemChanged(selectedPosition)
+                        selectedPosition = absoluteAdapterPosition
+                    }
+                }
             }
         }
     }
 
-    private fun selectTheme(theme: Theme) {
-        if (isSelected(theme)) {
-            selectedPositions.remove(theme.themeId)
-            //selectedThemeArray.removeAt(theme.themeId)
-            Timber.d("daily routine true -> ${selectedPositions}")
-            //Timber.d("daily routine true -> ${selectedThemeArray}")
-        } else {
-            selectedPositions.clear()
-            selectedPositions.add(theme.themeId)
-            //selectedThemeArray.add(theme.themeId)
-            Timber.d("daily routine false -> ${selectedPositions}")
-            //Timber.d("daily routine false -> ${selectedThemeArray}")
-        }
-    }
+//    private fun selectTheme(binding: ItemDailyRoutineAddThemeBinding, theme: Theme) {
+//        prePosition = selectedPosition - 1
+//
+////        prePosition = selectedPosition
+////
+////        fun updateSelectedPosition(theme: Theme) {
+////            selectedPosition = theme.themeId
+////            notifyItemChanged(selectedPosition)
+////            notifyItemChanged(prePosition)
+////            Timber.d("daily routine update -> ${selectedPosition}")
+////        }
+//
+//        if (isSelected(theme)) {
+//            Timber.d("daily routine true -> ${selectedPositions}")
+//        } else {
+//            selectedPositions.clear()
+//            selectedPositions.add(theme.themeId)
+//            changeThemeBackground(binding, true)
+//            Timber.d("daily routine false -> ${selectedPositions}")
+//
+//        }
+//    }
+
+    //selectedThemeArray.removeAt(theme.themeId)
+    //Timber.d("daily routine true -> ${selectedThemeArray}")
+    //selectedThemeArray.add(theme.themeId)
+    //Timber.d("daily routine false -> ${selectedThemeArray}")
 
     private fun changeThemeBackground(
         binding: ItemDailyRoutineAddThemeBinding,
@@ -98,3 +137,97 @@ class DailyRoutineAddThemeAdapter :
         holder.onBind(currentList[position])
     }
 }
+
+//    private val clickListener: ItemClickListener<Theme>
+//) : ListAdapter<Theme, DailyRoutineAddThemeAdapter.DailyThemeViewHolder>(DIFF_CALLBACK) {
+//    private var selectedPosition = RecyclerView.NO_POSITION
+//    private var prePosition = RecyclerView.NO_POSITION
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DailyThemeViewHolder {
+//        val itemDailyRoutineAddThemeBinding =
+//            ItemDailyRoutineAddThemeBinding.inflate(
+//                LayoutInflater.from(parent.context),
+//                parent,
+//                false
+//            )
+//        return DailyThemeViewHolder(itemDailyRoutineAddThemeBinding)
+//    }
+//
+//    @SuppressLint("ResourceAsColor")
+//    override fun onBindViewHolder(holder: DailyThemeViewHolder, position: Int) {
+//        holder.onBind(getItem(position), clickListener)
+//        prePosition = selectedPosition
+//        holder.binding.ivDailyRoutineAddThemeBackground.isSelected =
+//            (position == selectedPosition && position != RecyclerView.NO_POSITION)
+//        if (position == selectedPosition && position != RecyclerView.NO_POSITION) {
+//            holder.binding.tvDailyRoutineAddThemeName.setTextColor(gray700)
+//        } else {
+//            holder.binding.tvDailyRoutineAddThemeName.setTextColor(gray400)
+//        }
+//    }
+//
+//    private fun changeThemeBackground(
+//        binding: ItemDailyRoutineAddThemeBinding,
+//        selected: Boolean
+//    ) {
+//        when (selected) {
+//            true -> {
+//                binding.ivDailyRoutineAddThemeBackground.setBackgroundResource(R.drawable.ic_daily_theme_background_click)
+//                binding.tvDailyRoutineAddThemeName.setTextColor(
+//                    ContextCompat.getColor(
+//                        binding.root.context,
+//                        gray700
+//                    )
+//                )
+//            }
+//
+//            false -> {
+//                binding.ivDailyRoutineAddThemeBackground.setBackgroundResource(R.drawable.ic_daily_theme_background)
+//                binding.tvDailyRoutineAddThemeName.setTextColor(
+//                    ContextCompat.getColor(
+//                        binding.root.context,
+//                        gray400
+//                    )
+//                )
+//            }
+//        }
+//    }
+//
+//
+//    fun clearSelection() {
+//        prePosition = selectedPosition
+//        selectedPosition = RecyclerView.NO_POSITION
+//        notifyItemChanged(prePosition)
+//    }
+//
+//    fun updateSelectedPosition(position: Int) {
+//        selectedPosition = position
+//        notifyItemChanged(selectedPosition)
+//        notifyItemChanged(prePosition)
+//    }
+//
+//    class DailyThemeViewHolder(
+//        val binding: ItemDailyRoutineAddThemeBinding
+//    ) : RecyclerView.ViewHolder(binding.root) {
+//
+//        fun onBind(data: Theme, itemClickListener: ItemClickListener<Theme>) {
+//            binding.tvDailyRoutineAddThemeName.text = data.name
+//            binding.ivDailyRoutineAddThemeIcon.load(data.iconImageUrl)
+//            binding.root.setOnClickListener {
+//                itemClickListener.onClick(absoluteAdapterPosition, data)
+//            }
+//        }
+//    }
+//
+//    companion object {
+//        private val DIFF_CALLBACK = ItemDiffCallback<Theme>(
+//            onItemsTheSame = { old, new -> old.themeId == new.themeId },
+//            onContentsTheSame = { old, new -> old == new }
+//        )
+//    }
+//}
+//
+//fun interface ItemClickListener<T> {
+//    fun onClick(pos: Int, item: T)
+//}
+
