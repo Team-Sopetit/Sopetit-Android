63d2a5201ca78540d01efd0933b9d279bfbdda2e
heominhoe <hhmmm1005@naver.com>
2024-01-12 12:31:41 +0900

    #8 [feat] 리사이클러뷰 클릭 이벤트 적용

--- app/src/main/java/com/sopetit/softie/ui/dailyroutine/dailyroutineadd/DailyRoutineAddActivity.kt
+++ app/src/main/java/com/sopetit/softie/ui/dailyroutine/dailyroutineadd/DailyRoutineAddActivity.kt
@@ -11,6 +11,7 @@ import androidx.viewpager2.widget.ViewPager2
 import com.sopetit.softie.R
 import com.sopetit.softie.databinding.ActivityDailyRoutineAddBinding
 import com.sopetit.softie.util.binding.BindingActivity
+import timber.log.Timber
 
 class DailyRoutineAddActivity :
     BindingActivity<ActivityDailyRoutineAddBinding>(R.layout.activity_daily_routine_add) {
@@ -28,21 +29,21 @@ class DailyRoutineAddActivity :
 //        get() = requireNotNull(_dailyRoutineAddThemeAdapter)
 
 
-    private val dailyRoutineAddThemeViewModel by viewModels<DailyRoutineAddThemeViewModel>()
+    private val dailyRoutineAddViewModel by viewModels<DailyRoutineAddViewModel>()
+
+    val themeId = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewPager = binding.vpDailyRoutineAddCard
+        binding.viewModel = dailyRoutineAddViewModel
 
         setupAdapter()
-        //marginPage()
-        initViewPager()
+        setViewPager()
         setIndicator()
-        setDiv()
-        //initPagerDiv(19, 20)
+//        clickTheme()
+        setItemDiv()
     }
-
-    val themeId = 0
 
     private fun setupAdapter() {
         with(binding) {
@@ -51,91 +52,60 @@ class DailyRoutineAddActivity :
             dailyRoutineAddThemeAdapter = DailyRoutineAddThemeAdapter()
             rvDailyRoutineAddTheme.adapter = dailyRoutineAddThemeAdapter
         }
-        dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddThemeViewModel.mockDailyFirstCardList.value)
-        dailyRoutineAddThemeAdapter.submitList(dailyRoutineAddThemeViewModel.mockThemeList.value)
-    }
-
-    private fun clickTheme() {
-        dailyRoutineAddThemeAdapter.setOnThemeClickListener {
-
-        }
-    }
-
-
-    private fun marginPage() {
-        val pageMarginPx = resources.getDimensionPixelOffset(R.dimen.viewpager_margin)
-        val pagerWidth = resources.getDimensionPixelOffset(R.dimen.viewpager_margin)
-        val screenWidth = resources.displayMetrics.widthPixels
-        val offsetPx = screenWidth - pageMarginPx - pagerWidth
-
-        binding.vpDailyRoutineAddCard.setPageTransformer { page, position ->
-            page.translationX = position * -offsetPx
-        }
-    }
-
-    private fun initViewPager() {
-
-        viewPager.adapter = dailyRoutineAddCardPagerAdapter
-
-        val dpValue = 40
-        val d = resources.displayMetrics.density
-        val margin = (dpValue * d).toInt()
-
-
-        with(binding.vpDailyRoutineAddCard) {
-            clipChildren = false
-            clipToPadding = false
-            offscreenPageLimit = 3
-            setPadding(margin, 0, margin, 0)
-        }
-        val compositePageTransformer = CompositePageTransformer()
-        compositePageTransformer.addTransformer(
-            MarginPageTransformer(
-                resources.getDimensionPixelOffset(
-                    R.dimen.viewpager_margin
-                )
-            )
-        )
-        binding.vpDailyRoutineAddCard.setPageTransformer(compositePageTransformer)
-
-
-//        viewPager.apply {
-//            clipChildren = false
-//            clipToPadding = false
-//            offscreenPageLimit = 1
+//        with(dailyRoutineAddCardPagerAdapter) {
+//            submitList(dailyRoutineAddViewModel.mockDailyFirstCardList.value)
+////            submitList(dailyRoutineAddViewModel.mockDailySecondCardList.value)
+////            submitList(dailyRoutineAddViewModel.mockDailyThirdCardList.value)
+////            submitList(dailyRoutineAddViewModel.mockDailyFourthCardList.value)
+////            submitList(dailyRoutineAddViewModel.mockDailyFifthCardList.value)
 //        }
-//
+//        dailyRoutineAddThemeAdapter.submitList(dailyRoutineAddViewModel.mockThemeList.value)
 
+        dailyRoutineAddViewModel.mockThemeList.observe(this) {
+            dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailyFirstCardList.value)
+            dailyRoutineAddThemeAdapter.submitList(dailyRoutineAddViewModel.mockThemeList.value)
+        }
+        clickTheme()
+    }
+
+    private fun setViewPager() {
+        with(binding.vpDailyRoutineAddCard) {
+            adapter = dailyRoutineAddCardPagerAdapter
+            offscreenPageLimit = 3
+
+            val dpValue = 40
+            val margin = (dpValue * resources.displayMetrics.density).toInt()
+            setPadding(margin, 0, margin, 0)
+
+            setPageTransformer(CompositePageTransformer().apply {
+                addTransformer(MarginPageTransformer(resources.getDimensionPixelOffset(R.dimen.viewpager_margin)))
+            })
+        }
     }
 
     private fun setIndicator() {
         binding.diDailyRoutineAddIndicator.attachTo(binding.vpDailyRoutineAddCard)
     }
 
-    private fun initPagerDiv(previewWidth: Int, itemMargin: Int) {
-        val decoMargin = previewWidth + itemMargin
-        val pageTransX = decoMargin + previewWidth
-        val decoration = PageDecoration(decoMargin)
-
-        binding.vpDailyRoutineAddCard.also {
-            it.offscreenPageLimit = 1
-            it.addItemDecoration(decoration)
-            it.setPageTransformer { page, position ->
-                page.translationX = position * -pageTransX
+    private fun clickTheme() {
+        dailyRoutineAddThemeAdapter.setOnThemeClickListener {
+            Timber.d("daily routine act -> ${it.themeId}")
+            // Timber.d("daily routine act -> ${dailyRoutineAddThemeAdapter.selectedThemeArray}")
+            when (it.themeId) {
+                1 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailyFirstCardList.value)
+                2 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailySecondCardList.value)
+                3 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailyThirdCardList.value)
+                4 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailyFourthCardList.value)
+                5 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailyFifthCardList.value)
+                6 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailySixthCardList.value)
+                7 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailySeventhCardList.value)
+                8 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailyEighthCardList.value)
+                9 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailyNinthCardList.value)
+                10 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailyTenthCardList.value)
+                11 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailyEleventhCardList.value)
+                12 -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailyTwelfthCardList.value)
+                else -> dailyRoutineAddCardPagerAdapter.submitList(dailyRoutineAddViewModel.mockDailyFirstCardList.value)
             }
-        }
-    }
-
-    private class PageDecoration(private val margin: Int) : RecyclerView.ItemDecoration() {
-
-        override fun getItemOffsets(
-            outRect: Rect,
-            view: View,
-            parent: RecyclerView,
-            state: RecyclerView.State
-        ) {
-            outRect.left = margin
-            outRect.right = margin
         }
     }
 
@@ -153,7 +123,7 @@ class DailyRoutineAddActivity :
         }
     }
 
-    private fun setDiv() {
+    private fun setItemDiv() {
         binding.rvDailyRoutineAddTheme.addItemDecoration(HorizontalItemDecorator(16))
     }
 }
---
+++ app/src/main/java/com/sopetit/softie/ui/dailyroutine/dailyroutineadd/DailyRoutineAddViewModel.kt
@@ -0,0 +1,487 @@
+package com.sopetit.softie.ui.dailyroutine.dailyroutineadd
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.sopetit.softie.R
+import com.sopetit.softie.domain.entity.DailyCard
+import com.sopetit.softie.domain.entity.Theme
+
+class DailyRoutineAddViewModel : ViewModel() {
+    private val _mockThemeList: MutableLiveData<List<Theme>> = MutableLiveData(
+        mutableListOf(
+            Theme(
+                1,
+                "하루의 시작",
+                "https://www.urbanbrush.net/web/wp-content/uploads/edd/2022/12/urbanbrush-20221214144619159434.jpg",
+                ""
+            ),
+            Theme(
+                2,
+                "건강한 몸",
+                "https://www.urbanbrush.net/web/wp-content/uploads/edd/2022/12/urbanbrush-20221214144619159434.jpg",
+                ""
+            ),
+            Theme(
+                3,
+                "무기력 극복",
+                "https://www.urbanbrush.net/web/wp-content/uploads/edd/2022/12/urbanbrush-20221214144619159434.jpg",
+                ""
+            ),
+            Theme(
+                4,
+                "편안한 잠",
+                "https://www.urbanbrush.net/web/wp-content/uploads/edd/2022/12/urbanbrush-20221214144619159434.jpg",
+                ""
+            ),
+            Theme(
+                5,
+                "환경지킴이",
+                "https://www.urbanbrush.net/web/wp-content/uploads/edd/2022/12/urbanbrush-20221214144619159434.jpg",
+                ""
+            ),
+            Theme(
+                6,
+                "소소한 친절",
+                "https://www.urbanbrush.net/web/wp-content/uploads/edd/2022/12/urbanbrush-20221214144619159434.jpg",
+                ""
+            ),
+            Theme(
+                7,
+                "감사의 마음",
+                "https://www.urbanbrush.net/web/wp-content/uploads/edd/2022/12/urbanbrush-20221214144619159434.jpg",
+                ""
+            ),
+            Theme(
+                8,
+                "작은 성취감",
+                "https://www.urbanbrush.net/web/wp-content/uploads/edd/2022/12/urbanbrush-20221214144619159434.jpg",
+                ""
+            ),
+            Theme(
+                9,
+                "소중한 나",
+                "https://www.urbanbrush.net/web/wp-content/uploads/edd/2022/12/urbanbrush-20221214144619159434.jpg",
+                ""
+            ),
+            Theme(
+                10,
+                "통통한 통장",
+                "https://www.urbanbrush.net/web/wp-content/uploads/edd/2022/12/urbanbrush-20221214144619159434.jpg",
+                ""
+            ),
+            Theme(
+                11,
+                "몰입할 준비",
+                "https://www.urbanbrush.net/web/wp-content/uploads/edd/2022/12/urbanbrush-20221214144619159434.jpg",
+                ""
+            ),
+            Theme(
+                12,
+                "비워내기",
+                "https://www.urbanbrush.net/web/wp-content/uploads/edd/2022/12/urbanbrush-20221214144619159434.jpg",
+                ""
+            )
+
+        )
+    )
+
+    val mockThemeList: LiveData<List<Theme>>
+        get() = _mockThemeList
+
+    private val _mockDailyFirstCardList: MutableLiveData<List<DailyCard>> = MutableLiveData(
+        mutableListOf(
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 1,
+                content = "첫번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 2,
+                content = "첫번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 3,
+                content = "첫번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 4,
+                content = "첫번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 5,
+                content = "첫번째 데이터"
+            )
+        )
+    )
+
+    val mockDailyFirstCardList: LiveData<List<DailyCard>>
+        get() = _mockDailyFirstCardList
+
+    private val _mockDailySecondCardList: MutableLiveData<List<DailyCard>> = MutableLiveData(
+        mutableListOf(
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 6,
+                content = "두번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 7,
+                content = "두번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 8,
+                content = "두번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 9,
+                content = "두번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 10,
+                content = "두번째 데이터"
+            )
+        )
+    )
+
+    val mockDailySecondCardList: LiveData<List<DailyCard>>
+        get() = _mockDailySecondCardList
+
+    private val _mockDailyThirdCardList: MutableLiveData<List<DailyCard>> = MutableLiveData(
+        mutableListOf(
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 11,
+                content = "세번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 12,
+                content = "세번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 13,
+                content = "세번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 14,
+                content = "세번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 15,
+                content = "세번째 데이터"
+            )
+        )
+    )
+
+    val mockDailyThirdCardList: LiveData<List<DailyCard>>
+        get() = _mockDailyThirdCardList
+
+    private val _mockDailyFourthCardList: MutableLiveData<List<DailyCard>> = MutableLiveData(
+        mutableListOf(
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 1,
+                content = "네번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 2,
+                content = "네번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 3,
+                content = "네번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 4,
+                content = "네번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 5,
+                content = "네번째 데이터"
+            )
+        )
+    )
+
+    val mockDailyFourthCardList: LiveData<List<DailyCard>>
+        get() = _mockDailyFourthCardList
+
+    private val _mockDailyFifthCardList: MutableLiveData<List<DailyCard>> = MutableLiveData(
+        mutableListOf(
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 1,
+                content = "다섯번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 2,
+                content = "다섯번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 3,
+                content = "다섯번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 4,
+                content = "다섯번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 5,
+                content = "다섯번째 데이터"
+            )
+        )
+    )
+
+    val mockDailyFifthCardList: LiveData<List<DailyCard>>
+        get() = _mockDailyFifthCardList
+
+    private val _mockDailySixthCardList: MutableLiveData<List<DailyCard>> = MutableLiveData(
+        mutableListOf(
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 1,
+                content = "여섯번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 2,
+                content = "여섯번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 3,
+                content = "여섯번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 4,
+                content = "여섯번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 5,
+                content = "여섯번째 데이터"
+            )
+        )
+    )
+
+    val mockDailySixthCardList: LiveData<List<DailyCard>>
+        get() = _mockDailySixthCardList
+
+    private val _mockDailySeventhCardList: MutableLiveData<List<DailyCard>> = MutableLiveData(
+        mutableListOf(
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 1,
+                content = "일곱번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 2,
+                content = "일곱번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 3,
+                content = "일곱번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 4,
+                content = "일곱번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 5,
+                content = "일곱번째 데이터"
+            )
+        )
+    )
+
+    val mockDailySeventhCardList: LiveData<List<DailyCard>>
+        get() = _mockDailySeventhCardList
+
+    private val _mockDailyEighthCardList: MutableLiveData<List<DailyCard>> = MutableLiveData(
+        mutableListOf(
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 1,
+                content = "여덟번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 2,
+                content = "여덟번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 3,
+                content = "여덟번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 4,
+                content = "여덟번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 5,
+                content = "여덟번째 데이터"
+            )
+        )
+    )
+
+    val mockDailyEighthCardList: LiveData<List<DailyCard>>
+        get() = _mockDailyEighthCardList
+
+    private val _mockDailyNinthCardList: MutableLiveData<List<DailyCard>> = MutableLiveData(
+        mutableListOf(
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 1,
+                content = "아홉번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 2,
+                content = "아홉번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 3,
+                content = "아홉번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 4,
+                content = "아홉번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 5,
+                content = "아홉번째 데이터"
+            )
+        )
+    )
+
+    val mockDailyNinthCardList: LiveData<List<DailyCard>>
+        get() = _mockDailyNinthCardList
+
+    private val _mockDailyTenthCardList: MutableLiveData<List<DailyCard>> = MutableLiveData(
+        mutableListOf(
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 1,
+                content = "열번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 2,
+                content = "열번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 3,
+                content = "열번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 4,
+                content = "열번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 5,
+                content = "열번째 데이터"
+            )
+        )
+    )
+
+    val mockDailyTenthCardList: LiveData<List<DailyCard>>
+        get() = _mockDailyTenthCardList
+
+    private val _mockDailyEleventhCardList: MutableLiveData<List<DailyCard>> = MutableLiveData(
+        mutableListOf(
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 1,
+                content = "열한번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 2,
+                content = "열한번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 3,
+                content = "열한번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 4,
+                content = "열한번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 5,
+                content = "열한번째 데이터"
+            )
+        )
+    )
+
+    val mockDailyEleventhCardList: LiveData<List<DailyCard>>
+        get() = _mockDailyEleventhCardList
+
+    private val _mockDailyTwelfthCardList: MutableLiveData<List<DailyCard>> = MutableLiveData(
+        mutableListOf(
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 1,
+                content = "열두번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 2,
+                content = "열두번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 3,
+                content = "열두번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 4,
+                content = "열두번째 데이터"
+            ),
+            DailyCard(
+                card = R.drawable.shape_gray_fill_20_rect,
+                routineId = 5,
+                content = "열두번째 데이터"
+            )
+        )
+    )
+
+    val mockDailyTwelfthCardList: LiveData<List<DailyCard>>
+        get() = _mockDailyTwelfthCardList
+}
