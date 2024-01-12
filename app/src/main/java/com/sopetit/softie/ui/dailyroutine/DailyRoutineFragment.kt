0aa97ecfa62a0cf9b72e245752f5eaef4e49fa8d
KIM MINJEONG <minjeong030420@gmail.com>
2024-01-12 22:02:43 +0900

    #42 [mod] 데일리 루틴 삭제 xml 수정

--- app/src/main/java/com/sopetit/softie/ui/dailyroutine/DailyRoutineFragment.kt
+++ app/src/main/java/com/sopetit/softie/ui/dailyroutine/DailyRoutineFragment.kt
@@ -1,32 +1,28 @@
 package com.sopetit.softie.ui.dailyroutine
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
 import com.sopetit.softie.R
 import com.sopetit.softie.databinding.FragmentDailyRoutineBinding
-import com.sopetit.softie.ui.dailyroutine.dailyroutineedit.DailyRoutineEditActivity
 import com.sopetit.softie.util.binding.BindingFragment
 
 class DailyRoutineFragment :
     BindingFragment<FragmentDailyRoutineBinding>(R.layout.fragment_daily_routine) {
 
-    private val dailyRoutineViewModel by viewModels<DailyRoutineViewModel>()
+    private val viewModel by viewModels<DailyRoutineViewModel>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.lifecycleOwner = this
-        binding.dailyRoutineViewModel = dailyRoutineViewModel
+        binding.viewModel = viewModel
 
-        moveEdit()
+        initSetDeleteView()
     }
 
-    private fun moveEdit() {
+    private fun initSetDeleteView() {
         binding.tvDailyRoutineEdit.setOnClickListener {
-            val intent = Intent(requireActivity(), DailyRoutineEditActivity::class.java)
-            startActivity(intent)
+            viewModel.setDeleteView(true)
         }
     }
 }
