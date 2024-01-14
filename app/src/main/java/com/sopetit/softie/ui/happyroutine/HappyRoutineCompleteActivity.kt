45befdc332cb4f23dc302bc055c91278d2af8551
Hailey <91793891+stellar-halo@users.noreply.github.com>
2024-01-14 18:47:17 +0900

    #46 [ui] 행복 루틴 완료 시 보일 activity, (곰 타입에 따른 이미지 처리)

---
+++ app/src/main/java/com/sopetit/softie/ui/happyroutine/HappyRoutineCompleteActivity.kt
@@ -0,0 +1,26 @@
+package com.sopetit.softie.ui.happyroutine
+
+import android.os.Bundle
+import com.sopetit.softie.R
+import com.sopetit.softie.domain.entity.Bear
+import com.sopetit.softie.domain.entity.Cotton
+import com.sopetit.softie.util.RoutineCompleteActivity
+
+class HappyRoutineCompleteActivity : RoutineCompleteActivity(Cotton.HAPPINESS) {
+    lateinit var bear: Bear
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    private fun setHappyRoutineImage() {
+        val bearDrawableMap = mapOf(
+            Bear.BROWN to R.drawable.ic_bear_happy_complete_brown,
+            Bear.GRAY to R.drawable.ic_bear_happy_complete_gray,
+            Bear.RED to R.drawable.ic_bear_happy_complete_red,
+            Bear.PANDA to R.drawable.ic_bear_happy_complete_panda
+        )
+        val resourceId = bearDrawableMap[bear] ?: R.drawable.ic_bear_happy_complete_brown
+
+        binding.ivRoutineComplete.setImageResource(resourceId)
+    }
+}
