1a822c4e1612e7a1805ac8ef521f93ca8bd74c30
pump9918 <hoyeon9918@gmail.com>
2024-01-14 23:56:01 +0900

    #48 [ui] 추가하기 버튼 클릭 프래그먼트 연결

---
+++ app/src/main/java/com/sopetit/softie/ui/happyroutine/progress/HappyProgressFragment.kt
@@ -0,0 +1,14 @@
+package com.sopetit.softie.ui.happyroutine.progress
+
+import android.os.Bundle
+import android.view.View
+import com.sopetit.softie.R
+import com.sopetit.softie.databinding.FragmentHappyProgressBinding
+import com.sopetit.softie.util.binding.BindingFragment
+
+class HappyProgressFragment :
+    BindingFragment<FragmentHappyProgressBinding>(R.layout.fragment_happy_progress) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
