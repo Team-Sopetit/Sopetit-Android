1adbea0a0359b31459ebd68151aa95ca0fb334fe
KIM MINJEONG <minjeong030420@gmail.com>
2024-01-16 06:13:11 +0900

    #38 [mod] 테마 리스트 코드 수정

--- app/src/main/java/com/sopetit/softie/ui/onboarding/themechoice/ChoiceThemeAdapter.kt
+++ app/src/main/java/com/sopetit/softie/ui/onboarding/themechoice/ChoiceThemeAdapter.kt
@@ -4,12 +4,12 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
 import com.sopetit.softie.R
 import com.sopetit.softie.databinding.ItemOnboardingChoiceThemeBinding
 import com.sopetit.softie.domain.entity.Theme
 import com.sopetit.softie.ui.onboarding.OnboardingActivity.Companion.MAXIMUM_THEME_SELECTION
 import com.sopetit.softie.util.ItemDiffCallback
-import com.sopetit.softie.util.binding.BindingAdapter.setImage
 import com.sopetit.softie.util.toast
 
 class ChoiceThemeAdapter :
@@ -27,8 +27,8 @@ class ChoiceThemeAdapter :
         RecyclerView.ViewHolder(binding.root) {
         fun onBind(data: Theme) {
             binding.tvThemeName.text = data.name
-            binding.ivThemeIcon.setImage(data.iconImageUrl)
-//            binding.ivThemeIcon.load(data.iconImageUrl)
+//            binding.ivThemeIcon.setImage(data.iconImageUrl)
+            binding.ivThemeIcon.load(data.iconImageUrl)
             binding.root.setOnClickListener {
                 themeSelection(binding, data)
                 onItemClickListener?.let { it(data) }
--- app/src/main/java/com/sopetit/softie/ui/onboarding/themechoice/ChoiceThemeViewModel.kt
+++ app/src/main/java/com/sopetit/softie/ui/onboarding/themechoice/ChoiceThemeViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sopetit.softie.domain.entity.Theme
+import com.sopetit.softie.domain.usecase.GetDollUseCase
 import com.sopetit.softie.domain.usecase.GetThemeListUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -13,6 +14,7 @@ import javax.inject.Inject
 
 @HiltViewModel
 class ChoiceThemeViewModel @Inject constructor(
+    private val getDollUseCase: GetDollUseCase,
     private val getThemeListUseCase: GetThemeListUseCase
 ) : ViewModel() {
 
@@ -24,11 +26,28 @@ class ChoiceThemeViewModel @Inject constructor(
     val themeBtnEnabled: LiveData<Boolean>
         get() = _themeBtnEnabled
 
+    private val _bearFace: MutableLiveData<String> = MutableLiveData()
+    val bearFace: LiveData<String>
+        get() = _bearFace
+
     fun getThemeList() {
         viewModelScope.launch {
             getThemeListUseCase.invoke()
                 .onSuccess { response ->
                     _themeList.value = response
+                    Timber.d("서버 통신 성공 -> ${response}")
+                }.onFailure {
+                    Timber.e("서버 통신 실패 -> ${it.message}")
+                }
+        }
+    }
+
+    fun getBearFace(faceUrl: String) {
+        viewModelScope.launch {
+            getDollUseCase.invoke(faceUrl)
+                .onSuccess { response ->
+                    _bearFace.value = response
+                    Timber.d("서버 통신 성공 -> ${response}")
                 }.onFailure {
                     Timber.e("서버 통신 실패 -> ${it.message}")
                 }
