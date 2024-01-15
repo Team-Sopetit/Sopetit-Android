4868f8934642422676d97f24ffd721535d1be45b
Hailey <91793891+stellar-halo@users.noreply.github.com>
2024-01-15 21:12:44 +0900

    #58 [fix] response entity에 SerialName 붙이기

--- app/src/main/java/com/sopetit/softie/data/entity/response/HomeResponse.kt
+++ app/src/main/java/com/sopetit/softie/data/entity/response/HomeResponse.kt
@@ -1,15 +1,22 @@
 package com.sopetit.softie.data.entity.response
 
 import com.sopetit.softie.domain.entity.Home
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class HomeResponse(
+    @SerialName("name")
     val name: String,
+    @SerialName("dollType")
     val dollType: String,
+    @SerialName("frameImageUrl")
     val frameImageUrl: String,
+    @SerialName("dailyCottonCount")
     val dailyCottonCount: Int,
+    @SerialName("happinessCottonCount")
     val happinessCottonCount: Int,
+    @SerialName("conversations")
     val conversations: List<String>
 ) {
     fun toHome(): Home = Home(
