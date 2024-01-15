16354a71f201edc6915eabfce767b5d665a01428
Hailey <91793891+stellar-halo@users.noreply.github.com>
2024-01-15 21:09:42 +0900

    #58 [fix] base response에 serialName 추가

--- app/src/main/java/com/sopetit/softie/data/entity/BaseResponse.kt
+++ app/src/main/java/com/sopetit/softie/data/entity/BaseResponse.kt
@@ -1,10 +1,14 @@
 package com.sopetit.softie.data.entity
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class BaseResponse<T>(
+    @SerialName("success")
     val success: Boolean,
+    @SerialName("message")
     val message: String,
+    @SerialName("data")
     val data: T? = null
 )
