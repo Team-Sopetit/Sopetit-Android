923cbd8c6dba66729edaf7961645b5e2f7c87466
pump9918 <hoyeon9918@gmail.com>
2024-01-15 13:11:58 +0900

    #52 [ui] 카드 클릭 액티비티 연결

--- app/src/main/java/com/sopetit/softie/ui/happyroutine/HappyRoutineFragment.kt
+++ app/src/main/java/com/sopetit/softie/ui/happyroutine/HappyRoutineFragment.kt
@@ -1,14 +1,25 @@
 package com.sopetit.softie.ui.happyroutine
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import com.sopetit.softie.R
 import com.sopetit.softie.databinding.FragmentHappyRoutineBinding
+import com.sopetit.softie.ui.happyroutine.addlist.HappyAddListActivity
 import com.sopetit.softie.util.binding.BindingFragment
 
 class HappyRoutineFragment :
     BindingFragment<FragmentHappyRoutineBinding>(R.layout.fragment_happy_routine) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setCardEnter()
+    }
+
+    private fun setCardEnter() {
+        binding.ivHappyRoutineEmptyCard.setOnClickListener {
+            val intent = Intent(requireContext(), HappyAddListActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
