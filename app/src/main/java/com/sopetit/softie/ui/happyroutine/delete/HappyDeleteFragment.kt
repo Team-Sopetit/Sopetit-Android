69ce3a2f5619eabf1bd66cb75cb914ed8abbaa08
pump9918 <hoyeon9918@gmail.com>
2024-01-15 22:26:55 +0900

    #54 [ui] 삭제하기 바텀시트 버튼 스낵바

--- app/src/main/java/com/sopetit/softie/ui/happyroutine/delete/HappyDeleteFragment.kt
+++ app/src/main/java/com/sopetit/softie/ui/happyroutine/delete/HappyDeleteFragment.kt
@@ -3,31 +3,37 @@ package com.sopetit.softie.ui.happyroutine.delete
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import com.sopetit.softie.R
 import com.sopetit.softie.databinding.FragmentHappyDeleteBinding
-import com.sopetit.softie.domain.entity.HappyUser
+import com.sopetit.softie.domain.entity.HappyProgress
 import com.sopetit.softie.ui.happyroutine.HappyRoutineFragment
+import com.sopetit.softie.ui.happyroutine.progress.HappyProgressViewModel
 import com.sopetit.softie.util.OriginalBottomSheet
 import com.sopetit.softie.util.binding.BindingBottomSheet
 import com.sopetit.softie.util.binding.BindingFragment
+import com.sopetit.softie.util.snackBar
 
 class HappyDeleteFragment :
     BindingFragment<FragmentHappyDeleteBinding>(R.layout.fragment_happy_delete) {
 
+    private val viewModel by viewModels<HappyProgressViewModel>()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val viewModel: HappyDeleteViewModel by activityViewModels()
-        val happyDelete = viewModel.mockHappyDelete
+        val viewModel: HappyProgressViewModel by activityViewModels()
+        val happyDelete = viewModel.mockHappyProgress
         val routineId = 1
 
         setCardBinding(happyDelete)
         setCardEnter()
         setCancelEnter()
+        //initSetDeleteView()
         setClearEnter(routineId)
     }
 
-    private fun setCardBinding(happyDelete: HappyUser) {
+    private fun setCardBinding(happyDelete: HappyProgress) {
         with(binding) {
             tvHappyDeleteSubtitle.text = happyDelete.title
             ivHappyDeleteCardFront.setImageResource(happyDelete.imageUrl)
@@ -88,6 +94,11 @@ class HappyDeleteFragment :
                 doBtnColor = R.drawable.shape_red_fill_12_rect,
                 backBtnAction = {},
                 doBtnAction = {
+                    snackBar(
+                        binding.root.rootView,
+                        "행복루틴을 삭제했어요"
+                    )
+                    viewModel.setDeleteView(false)
                     requireActivity().supportFragmentManager.beginTransaction()
                         .replace(R.id.fcv_main, HappyRoutineFragment())
                         .commit()
