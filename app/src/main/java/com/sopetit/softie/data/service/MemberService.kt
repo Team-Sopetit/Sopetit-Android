33b955d846d86b4d16c27bace7e93b9f398d9dbb
Hailey <91793891+stellar-halo@users.noreply.github.com>
2024-01-16 11:19:44 +0900

    #65 [feat] patchCotton Service에 추가

--- app/src/main/java/com/sopetit/softie/data/service/MemberService.kt
+++ app/src/main/java/com/sopetit/softie/data/service/MemberService.kt
@@ -1,10 +1,15 @@
 package com.sopetit.softie.data.service
 
 import com.sopetit.softie.data.entity.BaseResponse
+import com.sopetit.softie.data.entity.response.CottonCountResponse
 import com.sopetit.softie.data.entity.response.HomeResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface MemberService {
     @GET("api/v1/members")
     suspend fun getHome(): BaseResponse<HomeResponse>
+
+    @GET("api/v1/members/{cottonType}")
+    suspend fun patchCotton(@Path("cottonType") cottonType: String): BaseResponse<CottonCountResponse>
 }
