361998b1bb754cb6f5fa9bc0916a446368b2860c
heominhoe <hhmmm1005@naver.com>
2024-01-16 20:34:23 +0900

    #66 [add] 데일리루틴 달성 리포지토리 생성

--- app/src/main/java/com/sopetit/softie/data/repositoryImpl/DailyRoutineRepositoryImpl.kt
+++ app/src/main/java/com/sopetit/softie/data/repositoryImpl/DailyRoutineRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.sopetit.softie.data.repositoryImpl
 
 import com.sopetit.softie.data.source.DailyRoutineDataSource
+import com.sopetit.softie.domain.entity.DailyAchieve
 import com.sopetit.softie.domain.entity.DailyRoutine
 import com.sopetit.softie.domain.repository.DailyRoutineRepository
 import javax.inject.Inject
@@ -11,5 +12,10 @@ class DailyRoutineRepositoryImpl @Inject constructor(
     override suspend fun getDailyRoutine(): Result<List<DailyRoutine>> =
         runCatching { dailyRoutineDataSource.getDailyRoutine() }.mapCatching { response ->
             requireNotNull(response.data).toUserDailyRoutine()
+        }
+
+    override suspend fun patchAchieveDaily(routineId: Int): Result<DailyAchieve> =
+        runCatching { dailyRoutineDataSource.patchAchieveDaily(routineId) }.map { response ->
+            requireNotNull(response.data).toAchieveDaily()
         }
 }
--- app/src/main/java/com/sopetit/softie/domain/repository/DailyRoutineRepository.kt
+++ app/src/main/java/com/sopetit/softie/domain/repository/DailyRoutineRepository.kt
@@ -1,7 +1,9 @@
 package com.sopetit.softie.domain.repository
 
+import com.sopetit.softie.domain.entity.DailyAchieve
 import com.sopetit.softie.domain.entity.DailyRoutine
 
 interface DailyRoutineRepository {
     suspend fun getDailyRoutine(): Result<List<DailyRoutine>>
+    suspend fun patchAchieveDaily(routineId: Int): Result<DailyAchieve>
 }
