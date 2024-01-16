f76b0f3a12509c89e1b198c4b7c0a6530843acbd
Hailey <91793891+stellar-halo@users.noreply.github.com>
2024-01-16 17:27:35 +0900

    #65 [fix] get -> patch로 Annotation 변경

--- app/src/main/java/com/sopetit/softie/data/service/MemberService.kt
+++ app/src/main/java/com/sopetit/softie/data/service/MemberService.kt
@@ -4,12 +4,13 @@ import com.sopetit.softie.data.entity.BaseResponse
 import com.sopetit.softie.data.entity.response.CottonCountResponse
 import com.sopetit.softie.data.entity.response.HomeResponse
 import retrofit2.http.GET
+import retrofit2.http.PATCH
 import retrofit2.http.Path
 
 interface MemberService {
     @GET("api/v1/members")
     suspend fun getHome(): BaseResponse<HomeResponse>
 
-    @GET("api/v1/members/{cottonType}")
+    @PATCH("api/v1/members/{cottonType}")
     suspend fun patchCotton(@Path("cottonType") cottonType: String): BaseResponse<CottonCountResponse>
 }
