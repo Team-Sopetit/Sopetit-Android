09714f7732acf291fe0fa0a76b42c0b6a39eb8c9
Hailey <91793891+stellar-halo@users.noreply.github.com>
2024-01-17 23:31:17 +0900

    #72 [feat] sharedPreference에 회원가입한 사용자인지 아닌지 boolean값 추가

--- app/src/main/java/com/sopetit/softie/data/source/LocalDataSource.kt
+++ app/src/main/java/com/sopetit/softie/data/source/LocalDataSource.kt
@@ -15,8 +15,13 @@ class LocalDataSource @Inject constructor(
         set(value) = prefs.edit { putString(REFRESH_TOKEN, value) }
         get() = prefs.getString(REFRESH_TOKEN, "") ?: ""
 
+    var isUserSignUp: Boolean
+        set(value) = prefs.edit { putBoolean(USER, value) }
+        get() = prefs.getBoolean(USER, false)
+
     companion object {
         private const val ACCESS_TOKEN = "access_token"
         private const val REFRESH_TOKEN = "refresh_token"
+        private const val USER = "user"
     }
 }
