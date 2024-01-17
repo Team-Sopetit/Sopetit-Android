b653161dce01f9eb1f5b7802915d2763c1d60e3c
KIM MINJEONG <minjeong030420@gmail.com>
2024-01-18 02:21:40 +0900

    #74 [fix] CI 오류 해결

--- app/src/main/java/com/sopetit/softie/data/service/AuthService.kt
+++ app/src/main/java/com/sopetit/softie/data/service/AuthService.kt
@@ -1,10 +1,10 @@
 package com.sopetit.softie.data.service
 
 import com.sopetit.softie.data.entity.BaseResponse
-import retrofit2.http.DELETE
 import com.sopetit.softie.data.entity.request.LoginRequest
 import com.sopetit.softie.data.entity.response.LoginResponse
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.POST
 
 interface AuthService {
