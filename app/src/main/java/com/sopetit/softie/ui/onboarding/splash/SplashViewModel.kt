1d22c037ac5e05853dfa771a76400383dcb52ed8
Hailey <91793891+stellar-halo@users.noreply.github.com>
2024-02-01 00:14:18 +0900

    #142 [ui] version에 따라 dialog가 다르게 뜨게 로직 추가

--- app/src/main/java/com/sopetit/softie/ui/onboarding/splash/SplashViewModel.kt
+++ app/src/main/java/com/sopetit/softie/ui/onboarding/splash/SplashViewModel.kt
@@ -1,13 +1,51 @@
 package com.sopetit.softie.ui.onboarding.splash
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.sopetit.softie.BuildConfig
+import com.sopetit.softie.domain.entity.UpdateType
 import com.sopetit.softie.domain.usecase.GetSignedUpUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import kotlin.math.pow
 
 @HiltViewModel
 class SplashViewModel @Inject constructor(
     private val getSignedUpUseCase: GetSignedUpUseCase
 ) : ViewModel() {
+    val versionName: String = BuildConfig.VERSION_NAME
+    private val _isUpdate: MutableLiveData<UpdateType> = MutableLiveData()
+    val isUpdate: LiveData<UpdateType> get() = _isUpdate
     fun isSignedUp(): Boolean = getSignedUpUseCase()
+
+    init {
+        isForceUpdate()
+    }
+
+    private fun isForceUpdate() {
+        val storeAppVersion = "1.0.2"
+        val forceAppVersion = "1.0.1"
+        val storeAppVersionInt = versionStringToNumber(storeAppVersion)
+        val forceAppVersionInt = versionStringToNumber(forceAppVersion)
+        val versionNameInt = versionStringToNumber(versionName)
+
+        if (storeAppVersionInt > forceAppVersionInt) {
+            if (versionNameInt < forceAppVersionInt) _isUpdate.value = UpdateType.FORCE
+            else _isUpdate.value = UpdateType.RECOMMEND
+        } else {
+            _isUpdate.value = UpdateType.NONE
+        }
+    }
+
+    private fun versionStringToNumber(version: String): Int {
+        val versionNumbers = version.split(".").map { it.toInt() }
+        var result = 0
+
+        for (i in versionNumbers.indices) {
+            result += versionNumbers[i] * 10.0.pow((versionNumbers.size - i - 1) * 2).toInt()
+        }
+
+        return result
+    }
 }
