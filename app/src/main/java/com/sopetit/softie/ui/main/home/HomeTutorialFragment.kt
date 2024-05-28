77919b1077b3ae05c8fd8ee46d95ea9ee96cc376
stellar-halo <stellarhalo@naver.com>
2024-05-28 19:37:04 +0900

    #173[ui] 하단 인디케이터 연결

--- app/src/main/java/com/sopetit/softie/ui/main/home/HomeTutorialFragment.kt
+++ app/src/main/java/com/sopetit/softie/ui/main/home/HomeTutorialFragment.kt
@@ -4,6 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
+import androidx.core.view.marginLeft
+import androidx.core.view.marginStart
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.sopetit.softie.R
@@ -15,6 +19,16 @@ class HomeTutorialFragment : BottomSheetDialogFragment() {
     private var _binding: FragmentHomeTutorialBottomsheetBinding? = null
     private val binding get() = requireNotNull(_binding)
     private lateinit var viewPager: ViewPager2
+    private var currentIndex = 0
+    private val pageCallback by lazy {
+        object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                currentIndex = position
+                updateIndicators()
+            }
+        }
+    }
     private lateinit var homeTutorialAdapter: HomeTutorialAdapter
 
     override fun onCreateView(
@@ -28,10 +42,10 @@ class HomeTutorialFragment : BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         viewPager = binding.vpTutorial
         homeTutorialAdapter = HomeTutorialAdapter()
         binding.vpTutorial.adapter = homeTutorialAdapter
+        binding.vpTutorial.registerOnPageChangeCallback(pageCallback)
         homeTutorialAdapter.submitList(
             listOf(
                 Tutorial(0, R.drawable.tutorial_daily),
@@ -39,6 +53,50 @@ class HomeTutorialFragment : BottomSheetDialogFragment() {
                 Tutorial(2, R.drawable.tutorial_cotton)
             )
         )
+        setupIndicators()
+        updateIndicators()
     }
 
+    private fun setupIndicators() {
+        val indicators = Array(homeTutorialAdapter.itemCount) {
+            ImageView(requireContext()).apply {
+                setImageDrawable(
+                    ContextCompat.getDrawable(
+                        requireContext(),
+                        R.drawable.ic_indicator_seleted
+                    )
+                )
+                layoutParams = ViewGroup.MarginLayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+                ).apply {
+                    setMargins(8, 0, 8, 0)
+                }
+            }
+        }
+        indicators.forEach { indicator ->
+            binding.llTutorialIndicator.addView(indicator)
+        }
+    }
+
+    private fun updateIndicators() {
+        for (i in 0 until binding.llTutorialIndicator.childCount) {
+            val imageView = binding.llTutorialIndicator.getChildAt(i) as ImageView
+            if (i == currentIndex) {
+                imageView.setImageDrawable(
+                    ContextCompat.getDrawable(requireContext(), R.drawable.ic_indicator_seleted)
+                )
+            } else {
+                imageView.setImageDrawable(
+                    ContextCompat.getDrawable(requireContext(), R.drawable.ic_indicator_unselected)
+                )
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+        viewPager.unregisterOnPageChangeCallback(pageCallback)
+    }
 }
