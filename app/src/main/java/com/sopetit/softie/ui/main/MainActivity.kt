4c30b12d03612aa481360446a2785f3b0b515e03
stellar-halo <stellarhalo@naver.com>
2024-06-17 15:00:21 +0900

    #175 [chore] text style 정리 및 daily routine 파일 삭제

--- app/src/main/java/com/sopetit/softie/ui/main/MainActivity.kt
+++ app/src/main/java/com/sopetit/softie/ui/main/MainActivity.kt
@@ -6,9 +6,9 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.sopetit.softie.R
 import com.sopetit.softie.databinding.ActivityMainBinding
-import com.sopetit.softie.ui.dailyroutine.DailyRoutineFragment
 import com.sopetit.softie.ui.happyroutine.HappyMyRoutineFragment
 import com.sopetit.softie.ui.main.home.HomeFragment
+import com.sopetit.softie.ui.ongoingroutine.OngoingRoutineFragment
 import com.sopetit.softie.util.binding.BindingActivity
 import com.sopetit.softie.util.setStatusBarColorFromResource
 import dagger.hilt.android.AndroidEntryPoint
@@ -28,7 +28,7 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
     private fun setBottomNavigationClickListener() {
         binding.bnvMain.setOnItemSelectedListener { item ->
             when (item.itemId) {
-                R.id.bottom_navigation_daily_routine -> changeFragment(DailyRoutineFragment())
+                R.id.bottom_navigation_daily_routine -> changeFragment(OngoingRoutineFragment())
                 R.id.bottom_navigation_home -> changeFragment(HomeFragment())
                 R.id.bottom_navigation_happiness_routine -> changeFragment(HappyMyRoutineFragment())
                 else -> true
